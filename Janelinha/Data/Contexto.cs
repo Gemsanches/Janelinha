7e22a7e6c06deb6e227e8e0b85ebd1a75165a9de
Guilherme Sanches <gemsanches@hotmail.com>
2022-02-02 14:01:03 -0300

    Instalação e configuração inical da Entity

---
+++ Janelinha/Data/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Janelinha.Cadastros;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Janelinha.Data
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DBJanekinha; Integrated Security=True;");
+        }
+        DbSet<Cliente> Cliente { get; set; }
+    }
+}
---
+++ Janelinha/Migrations/20220202165820_InitialDataBase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Janelinha.Migrations
+{
+    public partial class InitialDataBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cliente");
+        }
+    }
+}
